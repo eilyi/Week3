9dc833f7740a8307f86271fe1bd6f9e1b8a3482d
Eily K Irwin <eilyi@unr.edu>
2019-09-12 20:07:13 -0700

    first commit

---
+++ stringsandlists.py
@@ -0,0 +1,16 @@
+#Strings and Lists 
+s= "This is a long string, oh boy"
+l= [1, 2, 3, 4]
+
+for number in l:
+    print(number)
+  
+for letter in s:
+    print(letter)   #newlinecharacter
+    print("\n")
+    
+l.append(121)
+print(1) 
+
+s.append("France")
+print(s)
